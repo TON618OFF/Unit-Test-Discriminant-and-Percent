77444b5a6dfd9d9309cd6937c2861415681cf3de
TON618OFF <rusblackgod@gmail.com>
2024-10-11 13:03:51 +0300

    fix delta check & class initialize

--- DiscrProcent.Tests/DiscriminantProcentTests.cs
+++ DiscrProcent.Tests/DiscriminantProcentTests.cs
@@ -1,23 +1,32 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
+using System.Collections.Generic;
 
 namespace DiscrProcent.Tests
 {
     [TestClass]
     public class DiscriminantProcentTests
     {
-        private static double a;
-        private static double b;
-        private static double c;
+        private static List<double[]> peremenie;
+        private static double num;
+        private static double procent;
         private static double expectedPercentage;
+        private static double delta;
 
         [ClassInitialize]
         public static void Setup(TestContext context)
         {
-            a = 2;
-            b = 3;
-            c = 6;
-            expectedPercentage = 25; 
+
+            peremenie = new List<double[]>
+            {
+                new double[] { 1, 2, 5 },
+                new double[] { 1, 2, 1 },
+                new double[] { 1, -3, 2 }
+            };
+            num = 100.00;
+            procent = 24.999;
+            expectedPercentage = 25.00;
+            delta = 0.001;
         }
 
         [TestMethod]
@@ -26,42 +35,52 @@ namespace DiscrProcent.Tests
             // arrange
             // act
             // assert
-            CollectionAssert.AreEqual(new double[0], DiscriminantProcent.CorniDiscriminanta(a, b, c), "Ожидалось, что корней нет (D < 0) для a = {0}, b = {1}, c = {2}", a, b, c);
+            CollectionAssert.AreEqual(new double[] { }, 
+                DiscriminantProcent.CorniDiscriminanta(peremenie[0][0], peremenie[0][1], peremenie[0][2]), 
+                "Ожидалось, что корней нет (D < 0) для a = {0}, b = {1}, c = {2}", peremenie[0][0], peremenie[0][1], peremenie[0][2]);
+
         }
 
         [TestMethod]
         public void FindSqrt_DiscrAreEqualZero_1SqrtReturn()
         {
             // arrange
-            double a = 1, b = 2, c = 1;
             // act
             // assert
-            CollectionAssert.AreEqual(new double[] { -1 }, DiscriminantProcent.CorniDiscriminanta(a, b, c), "Ожидалось, что корень один (D = 0) для a = {0}, b = {1}, c = {2}", a, b, c);
+            CollectionAssert.AreEquivalent(new double[] { -1 },
+                DiscriminantProcent.CorniDiscriminanta(peremenie[1][0], peremenie[1][1], peremenie[1][2]),
+                "Ожидалось, что корней нет (D = 0) для a = {0}, b = {1}, c = {2}", peremenie[1][0], peremenie[1][1], peremenie[1][2]);
+
         }
 
         [TestMethod]
         public void FindSqrt_DiscrMoreThanZero_2SqrtReturn()
         {
             // arrange
-            double a = 1, b = -3, c = 2;
             // act
+            double[] corni = DiscriminantProcent.CorniDiscriminanta(peremenie[2][0], peremenie[2][1], peremenie[2][2]);
             // assert
-            CollectionAssert.AreEqual(new double[] { 2, 1 }, DiscriminantProcent.CorniDiscriminanta(a, b, c), "Ожидалось, что корней два (D > 0) для a = {0}, b = {1}, c = {2}", a, b, c);
+            CollectionAssert.AllItemsAreNotNull(corni);
+            CollectionAssert.AllItemsAreUnique(corni);
+            CollectionAssert.AreEqual(new double[] { 2, 1 },
+                DiscriminantProcent.CorniDiscriminanta(peremenie[2][0], peremenie[2][1], peremenie[2][2]),
+                "Ожидалось, что корней нет (D > 0) для a = {0}, b = {1}, c = {2}", peremenie[2][0], peremenie[2][1], peremenie[2][2]);
         }
 
         [TestMethod]
         public void CalcProcent_ValidInputs_CorrectResultReturn()
         {
             // arrange
-            double num = 100;
-            double procent = 25;
-            double delta = 0.0001;
-
             // act
+            if (delta == 0)
+            {
+                Assert.Inconclusive("Тест не может быть выполнен, так как дельта не указана или равна нулю.");
+            }
             double result = DiscriminantProcent.CalcProcent(num, procent);
-
             // assert
-            Assert.AreEqual(25, result, delta, "Процент от числа {0} должен быть равен {1}%, вместо {2}%", num, expectedPercentage, procent);
+            Assert.AreEqual(expectedPercentage, result, delta, "Процент от числа {0} должен быть равен {1}%, вместо {2}%",
+            num, expectedPercentage, procent);
+
         }
     }
 }
